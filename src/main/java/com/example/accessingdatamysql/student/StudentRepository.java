da1d3a623768716faee7a831acb7ba85da34907c
Godefroy Diakite <godefroy.dkt@gmail.com>
2023-10-29 14:21:01 +0000

    done

--- src/main/java/com/example/accessingdatamysql/student/StudentRepository.java
+++ src/main/java/com/example/accessingdatamysql/student/StudentRepository.java
@@ -2,14 +2,14 @@ package com.example.accessingdatamysql.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import org.springframework.stereotype.Repository; 
+
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 
-@Repository
+
 public interface StudentRepository 
         extends JpaRepository<Student, Long> {
 
